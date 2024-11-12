08a06e1a1cf0be520140b7095a8947211e473f29
David Benoit <mdavid.benoit@gmail.com>
2024-11-12 13:09:52 -0600

    Listado de camiones completado

---
+++ Catalogos/Camiones/listar_Camiones.aspx.cs
@@ -0,0 +1,60 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Transportes_3_capas_gen_12.Catalogos.Camiones
+{
+    public partial class listar_Camiones : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //la variable IsPostBack representa la primera vez que carga la página
+            if (!IsPostBack)
+            {
+                cargarGrid();
+            }
+        }
+
+        public void cargarGrid()
+        {
+            //carga la información desde la BLL al GV
+            GVCamiones.DataSource = BLL_Camiones.Get_Camiones();
+            //Mostramos los resultados renderizando la información
+            GVCamiones.DataBind();
+        }
+
+        protected void Insertar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("formulariocamiones.aspx");
+        }
+
+        protected void GVCamiones_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+        }
+
+        protected void GVCamiones_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+        }
+
+        protected void GVCamiones_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+
+        protected void GVCamiones_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+        }
+
+        protected void GVCamiones_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
+        }
+    }
+}
