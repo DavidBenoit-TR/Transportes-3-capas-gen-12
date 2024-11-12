defd8a52360891dc4bf0675a60138135c16202c3
David Benoit <mdavid.benoit@gmail.com>
2024-11-12 13:41:05 -0600

    Bll_camiones completado

--- BLL/BLL_Camiones.cs
+++ BLL/BLL_Camiones.cs
@@ -10,10 +10,27 @@ namespace BLL
 {
     public class BLL_Camiones
     {
+        //Create
+        public static string insertar_Camion(Camiones_VO camion)
+        {
+            return DAL_Camiones.insertar_Camion(camion);
+        }
         //Read
         public static List<Camiones_VO> Get_Camiones(params object[] parametros)
         {
             return DAL_Camiones.Get_Camiones(parametros);
+        }
+
+        //Update
+        public static string actualizar_Camion(Camiones_VO camion)
+        {
+            return DAL_Camiones.actualizar_Camion(camion);
+        }
+
+        //Delete
+        public static string eliminar_Camion(int id)
+        {
+            return DAL_Camiones.eliminar_Camion(id);
         }
     }
 }
