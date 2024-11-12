5007b7827c7d92e7f9f9d80003db8b72a7bd3fa4
David Benoit <mdavid.benoit@gmail.com>
2024-11-12 09:37:25 -0600

    DAL_Camiones(Read) + Camiones_VO

---
+++ DAL/DAL_Camiones.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace DAL
+{
+    public class DAL_Camiones
+    {
+        //Create
+
+        //Read
+        public static List<Camiones_VO> Get_Camiones(params object[] parametros)
+        {
+            //creo una lista de objetos tipo VO
+            List<Camiones_VO> list = new List<Camiones_VO>();
+            try
+            {
+                //crear un dataset el cual recibirá lo que devuelva la ejecución del método "execte_dataset" que proviene de la clase "metodos_Datos"
+                DataSet ds_camiones = metodos_datos.execute_DataSet("", parametros);
+                //recorremos cada renglón existente de nuesro ds creando objetos de tipo VO y añadiéndilos a una lista
+                foreach (DataRow dr in ds_camiones.Tables[0].Rows)
+                {
+                    list.Add(new Camiones_VO(dr));
+                }
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        //Update
+
+        //Delete
+    }
+}
