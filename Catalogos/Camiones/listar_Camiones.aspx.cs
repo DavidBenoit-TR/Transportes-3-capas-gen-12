87ee6e2f64d57cce634e9094f43d2d1444c4eb75
David Benoit <mdavid.benoit@gmail.com>
2024-11-13 12:35:36 -0600

    CRUD Completo de Camiones (solo funcionalidad)

--- Catalogos/Camiones/listar_Camiones.aspx.cs
+++ Catalogos/Camiones/listar_Camiones.aspx.cs
@@ -34,12 +34,42 @@ namespace Transportes_3_capas_gen_12.Catalogos.Camiones
 
         protected void GVCamiones_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
+            //recupero el ID del renglón efectao
+            int idcamion = int.Parse(GVCamiones.DataKeys[e.RowIndex].Values["ID_Camion"].ToString());
+            //Invoco mi metodo para eliminar camiones desde la BLL
+            string respuesta = BLL_Camiones.eliminar_Camion(idcamion);
+            //preparamos el Sweet Alert
+            string titulo, msg, tipo;
+            if (respuesta.ToUpper().Contains("ERROR"))
+            {
+                titulo = "Error";
+                msg = respuesta;
+                tipo = "error";
+            }
+            else
+            {
+                titulo = "Correcto!";
+                msg = respuesta;
+                tipo = "success";
+            }
 
+            //sweet alert
+            //recargamos la página
+            cargarGrid();
         }
 
         protected void GVCamiones_RowCommand(object sender, GridViewCommandEventArgs e)
         {
-
+            //Defino si el comando (el click que se detecta) tiene la propiedad "Select"
+            if (e.CommandName == "Select")
+            {
+                //recupero el índice en función de aquel elemento que haya detonado el evento
+                int varIndex = int.Parse(e.CommandArgument.ToString());
+                //recupero el ID en función del índice que recueramos anteriormente
+                string id = GVCamiones.DataKeys[varIndex].Values["ID_Camion"].ToString();
+                //redirecciono al formulario de edición, pasando como parámetro el ID
+                Response.Redirect($"formulariocamiones.aspx?Id={id}");
+            }
         }
 
         protected void GVCamiones_RowEditing(object sender, GridViewEditEventArgs e)
